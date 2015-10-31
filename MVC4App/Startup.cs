c9924ba889d110f21b90245f35c7a30f028be04f
ZhangJin <nbnbnb.zhang@live.com>
2015-10-31 20:30:38 +0800

    MVC4App Init

---
+++ MVC4App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC4App.Startup))]
+namespace MVC4App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
